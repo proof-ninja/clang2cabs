d0d76bc2e1481756e31de232fe7a754e7cfebf96
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2021-12-23 18:33:32 +0900

    introduce sample yojson files

---
+++ sample/comments.c
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) proof ninja, Inc. and its affiliates.
+ */
+/**  I'm a long comment */
+void foo() {
+  /*  I'm a short comment */
+  // I'm a c++ comment
+}
---
+++ sample/hoge.c
@@ -0,0 +1,4 @@
+int hoge(int x) {
+  int y = x + 1;
+  return y;
+}
